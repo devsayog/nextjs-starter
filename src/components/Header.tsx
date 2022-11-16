b7fe966cd8c8f47c67de9160d578004cdf86a10f
devsayog <devsayog@gmail.com>
2022-11-16 14:32:50 +0545

    chore: add tailwindcss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="bg-gray-900 py-8 text-gray-100">
+      <h1 className="text-3xl text-red-500">Header</h1>
     </header>
   )
 }
